886dba5be16f614d1bc387a1df26e076fb11ecb0
essthere <omerleghenda@gmail.com>
2016-09-30 01:07:34 -0500

    Couple function was added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWold.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWold.java
@@ -2,9 +2,28 @@ package ru.stqa.pft.sandbox;
 
 public class HelloWold {
 
-	public static void main (String[] args){
+	public static void main (String[] args) {
+        hello("world");
+        hello("user");
+        hello("Natali");
 
-   System.out.println("Hello World");
+
+        double l = 5;
+        System.out.println("Ploshchad kvardata so storonoi" + 1 + "=" + area(l));
+
+        double a=4;
+        double b=6;
+
+        System.out.println("Ploshchad preamougolnika so storonami" + a + "i" + b + "=" + area(a, b));
+    }
+    public  static void hello (String somebody ) {
+        System.out.println("Hello,"+somebody + "!");
+    }
+     public static double area (double len){
+         return len * len;
+     }
+   public static double area (double a, double b){
+    return a * b;
+   }
 }
 
-}
